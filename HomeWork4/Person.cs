df175c7cef2ca2d86d4469dcfe1622f78669a372
Maksym-Semenov <mestcomua@gmail.com>
2022-07-03 00:54:04 +0300

    Добавьте файлы проекта.

---
+++ HomeWork4/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HomeWork4
+{
+    internal class Person
+    {
+        static string name;
+        static int birthdayYear;
+        int age;
+        public string Name { get; set; }
+        public int BirthdayYear { get; set; }
+
+        public Person()
+        {
+            name = "";
+            birthdayYear = 1900;
+        }
+        public Person(string name, int birthdayYear)
+        {
+            Person.name = name;
+            Person.birthdayYear = birthdayYear;
+        }
+        public static Person Input(int i)
+        {
+            Person person = new Person();
+            Console.Write("Input person name:\t");
+            name = Console.ReadLine();
+            Console.Write("Input person birthday year:\t");
+            birthdayYear = int.Parse(Console.ReadLine());
+            return person;
+        }
+        public int Age(int birthdayYear)
+        {
+            age = DateTime.Now.Year - birthdayYear;
+            return age;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"Info about person: name is {name}, age is {age}");
+        }
+    }
+}
---
+++ HomeWork4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number of person:\t");
+            int number = int.Parse(Console.ReadLine());
+            Person person = new Person();
+            var person = new Person[number];
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i] = Person.Input(i);
+            }
+            for (int i = 0; i < person.Length; i++)
+            {
+                person[i].Output();
+            }
+            Console.ReadLine();
+        }
+    }
+}
